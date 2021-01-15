63ced0564781b378d4063043ddaefdd18d3a5018
pakkawat2112 <pakkawat.su@ovec.moe.go.th>
2021-01-15 14:40:14 +0700

    edit index

--- index.php
+++ index.php
@@ -3,4 +3,6 @@ echo "My page";
 ?>
 <a href="page1.php">page1</a>
 <a href="page2.php">page2</a>
-<a href="page3">page3</a>
+<a href="page3.php">page3</a>
+<a href="page4.php">page4</a>
+<a href="page5.php">page5</a>
